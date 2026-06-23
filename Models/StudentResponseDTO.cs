18871dbf38847b6340b583b9d363f59e1fa09957
MuhammadTaha5 <taha.saeed339@gmail.com>
2026-06-23 14:14:45 +0500

    Set the DTO

--- Models/StudentResponseDTO.cs
+++ Models/StudentResponseDTO.cs
@@ -2,7 +2,6 @@ namespace MyFirstAPI.Models
 {
     public class StudentResponseDTO
     {
-        public int Id{ get; set;}
         public string? Name{set; get;}
         public int Age { get; set; }
         public string Department { get; set; } = string.Empty;
